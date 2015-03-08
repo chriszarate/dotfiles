82a107ff809b253060ce33e8ef0b04b8729da963
chriszarate <chris@zarate.org>
2015-03-08 15:07:59 -0400

    JSCS.

--- bin/create_links.sh
+++ bin/create_links.sh
@@ -5,11 +5,11 @@ dotfiles=".dotfiles"
 
 # Create symbolic links.
 cd ~
-ln -s $dotfiles/ansible/ansible.cfg .ansible.cfg
 ln -s $dotfiles/bash/bash_aliases .bash_aliases
 ln -s $dotfiles/bash/bash_profile .bash_profile
 ln -s $dotfiles/bash/bashrc .bashrc
 ln -s $dotfiles/git/gitconfig .gitconfig
 ln -s $dotfiles/git/gitignore_global .gitignore_global
 ln -s $dotfiles/jshint/jshintrc .jshintrc
+ln -s $dotfiles/jshint/jscsrc .jscsrc
 ln -s $dotfiles/tmux/tmux.conf .tmux.conf
