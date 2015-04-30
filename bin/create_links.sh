918d11c2749aa191000c0645b3d197c43649d709
chriszarate <chris@zarate.org>
2015-04-30 00:40:16 -0400

    EditorConfig

--- bin/create_links.sh
+++ bin/create_links.sh
@@ -8,6 +8,7 @@ cd ~
 ln -s $dotfiles/bash/bash_aliases .bash_aliases
 ln -s $dotfiles/bash/bash_profile .bash_profile
 ln -s $dotfiles/bash/bashrc .bashrc
+ln -s $dotfiles/editor/editorconfig .editorconfig
 ln -s $dotfiles/git/gitconfig .gitconfig
 ln -s $dotfiles/git/gitignore_global .gitignore_global
 ln -s $dotfiles/jshint/jshintrc .jshintrc
