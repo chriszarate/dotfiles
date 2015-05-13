92b1bd3fa5a5435d8f2284019ea5135f398802e0
chriszarate <chris@zarate.org>
2015-05-12 21:56:44 -0400

    Vim bindings in shell.

--- bin/create_links.sh
+++ bin/create_links.sh
@@ -9,6 +9,7 @@ ln -s $dotfiles/bash/bash_aliases .bash_aliases
 ln -s $dotfiles/bash/bash_profile .bash_profile
 ln -s $dotfiles/bash/bashrc .bashrc
 ln -s $dotfiles/editor/editorconfig .editorconfig
+ln -s $dotfiles/input/inputrc .inputrc
 ln -s $dotfiles/git/gitconfig .gitconfig
 ln -s $dotfiles/git/gitignore_global .gitignore_global
 ln -s $dotfiles/jshint/jshintrc .jshintrc
