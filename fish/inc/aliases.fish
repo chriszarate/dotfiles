8486ebab0ca1d011c2208e4cee1bff428f7ac04a
chriszarate <chris@zarate.org>
2015-06-19 12:04:36 -0400

    Add some git aliases to fish.

--- fish/inc/aliases.fish
+++ fish/inc/aliases.fish
@@ -16,10 +16,22 @@ alias ll "ls -laF | less"
 # Enable aliases to be sudo’ed
 alias sudo "sudo "
 
-# Git aliases
+# Git utilities
 alias __is_git "git rev-parse --is-inside-work-tree >/dev/null 2>&1"
 alias __git_origin "git remote -v | sed -E -e 's/^origin.*github.com[:\/]([^\.]+).git.*/\1/' -e '1!d'"
-alias gst "git status --short --branch"
+
+# Git user aliases
+alias ]f  "git fetch"
+alias ]p  "git pull"
+alias ]pu "git push"
+alias ]s  "git status --short --branch"
+alias ]a  "git add"
+alias ]ap "git add --patch"
+alias ]c  "git commit"
+alias ]d  "git diff"
+alias ]dc "git diff --cached"
+alias ]l  "git log --oneline"
+alias ]ll "git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 
 # Stopwatch
 alias timer 'echo "Timer started. Stop with Ctrl-D."; date; time cat; date'
