711c382cfc8245d1671fbca9b8d048202617287c
chriszarate <chris@zarate.org>
2015-06-23 12:19:05 -0400

    Add stashes to git prompt (they seem to be omitted from show_informative_status).

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,6 +1,5 @@
 set __fish_git_prompt_describe_style 'branch'
 set __fish_git_prompt_show_informative_status true
-set __fish_git_prompt_showupstream 'verbose'
 
 set __fish_git_prompt_color 666666
 set __fish_git_prompt_color_bare blue
@@ -10,7 +9,6 @@ set __fish_git_prompt_color_dirtystate red
 set __fish_git_prompt_color_invalidstate red
 set __fish_git_prompt_color_merging red
 set __fish_git_prompt_color_stagedstate green
-set __fish_git_prompt_color_stashstate blue
 set __fish_git_prompt_color_untrackedfiles red
 set __fish_git_prompt_color_upstream yellow
 
@@ -18,7 +16,6 @@ set __fish_git_prompt_char_cleanstate ''
 set __fish_git_prompt_char_dirtystate ' ●'
 set __fish_git_prompt_char_invalidstate ' ✗'
 set __fish_git_prompt_char_stagedstate ' ●'
-set __fish_git_prompt_char_stashstate ' ⚑'
 set __fish_git_prompt_char_stateseparator ''
 set __fish_git_prompt_char_untrackedfiles ' ○'
 set __fish_git_prompt_char_upstream_ahead ' ▲'
@@ -31,7 +28,19 @@ function fish_prompt
   set -l last_status $status
   set_color green
   printf '%s' (prompt_pwd)
-  printf '%s' (__fish_git_prompt ' @ %s ')
+  printf '%s' (__fish_git_prompt_wrapper)
   set_color 666666
-  printf '> '
+  printf ' > '
+end
+
+function __fish_git_prompt_wrapper
+  set -l git_prompt (__fish_git_prompt ' @ %s')
+  echo $git_prompt
+  if [ "$git_prompt" != '' ]
+    set -l git_stash (git stash list 2>/dev/null | wc -l | awk '{print $1}')
+    if [ "$git_stash" != '0' ]
+      set_color blue
+      echo " ⚑$git_stash"
+    end
+  end
 end
