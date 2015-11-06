8d3317463f9f57394c956cc2ff1b1a7d80b5c2d4
chriszarate <chris@zarate.org>
2015-11-06 10:44:39 -0500

    Extract SSH hostname from commands and refactor to switch statement.

--- fish/functions/fish_title.fish
+++ fish/functions/fish_title.fish
@@ -1,11 +1,24 @@
 function fish_title
-  if [ "fish" != "$_" ]
-    echo "$_"
-  else
-    if [ "$PWD" = "$HOME" ]
-      echo "~"
-    else
-      echo (basename $PWD)
-    end
+
+  switch $_
+
+    # If we're sitting a prompt, use the current working directory.
+    case fish
+      # If we're in the home directory, use "~" as shorthand.
+      if [ "$PWD" = "$HOME" ]
+        echo "~"
+      else
+        echo (basename $PWD)
+      end
+
+    # If we're SSHing, attempt to extract the hostname.
+    case ssh
+      echo "$argv[1]" | awk '{ for(i=NF; i>0; i--) { if(match($i, /^[^\-]/)) {print $i; break} } }'
+
+    # Otherwise just use the command name.
+    case '*'
+      echo "$_"
+
   end
+
 end
