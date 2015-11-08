7889bfd0cd8d15b093c9fbea41e96f5dcdcf93ce
chriszarate <chris@zarate.org>
2015-11-08 18:39:26 -0500

    Remove right square bracket git aliases and add to gitconfig.

--- fish/inc/aliases.fish
+++ fish/inc/aliases.fish
@@ -16,22 +16,6 @@ alias D="cd ~/Downloads"
 alias L="cd ~/Library"
 alias S="cd ~/Documents/Scratch"
 
-# Git user aliases
-alias ]co "git checkout"
-alias ]f "git fetch"
-alias ]p  "git pull"
-alias ]pu "git push"
-alias ]s  "git status --short --branch"
-alias ]b  "git branch"
-alias ]a  "git add"
-alias ]ap "git add --patch"
-alias ]c  "git commit"
-alias ]ca "git commit --amend"
-alias ]d  "git diff"
-alias ]dc "git diff --cached"
-alias ]l  "git log --oneline"
-alias ]ll "git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
-
 # Show running EC2 instances in a nice table (uses aws-cli)
 alias ec2show "aws ec2 describe-instances --query 'Reservations[*].Instances[*].{Name:Tags[?Key==`Name`]|[0].Value, State:State.Name, ID:InstanceId, Type:InstanceType, Storage:RootDeviceType, Zone:Placement.AvailabilityZone, PublicIP:PublicIpAddress}' --output table"
 
