32433a80bddcbfc40786f6d56e59eb36a9cf405c
chriszarate <chris@zarate.org>
2015-11-17 22:54:39 -0500

    Fix for repo URLs without trailing '.git'.

--- fish/functions/git-origin.fish
+++ fish/functions/git-origin.fish
@@ -1,4 +1,4 @@
 function git-origin -d 'Extract a shorthand form of the remote Git origin, if it exists.'
   git rev-parse --is-inside-work-tree >/dev/null 2>&1
-  and git remote -v | sed -E -e 's/^origin[[:space:]]([^ ]+).*$/\1/gp;d' | sed -E -e '1!d' -e 's/^git@github\.com:(.+)\.git$/\1/' -e 's/^https:\/\/github\.com\/(.+)\.git$/HTTPS \1/'
+  and git remote -v | sed -E -e 's/^origin[[:space:]]([^ ]+).*$/\1/gp;d' | sed -E -e '1!d' -e 's/^git@github\.com:(.+)$/\1/' -e 's/^https:\/\/github\.com\/(.+)$/\1/' -e 's/\.git$//'
 end
