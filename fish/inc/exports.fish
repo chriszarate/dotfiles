815e406041473a15c5f7aaad30232c8968b55223
chriszarate <chris@zarate.org>
2017-04-14 13:27:10 -0400

    so insensitive

--- fish/inc/exports.fish
+++ fish/inc/exports.fish
@@ -2,5 +2,5 @@
 set -x "DOCKER_LOCAL_IP" (ifconfig | grep "inet " | grep -v -m 1 127.0.0.1 | awk '{print $2}')
 
 # Export AWS credentials from ~/.aws/credentials
-set -x "AWS_ACCESS_KEY_ID" (grep -m 1 'aws_access_key_id' ~/.aws/credentials | sed 's/.*= *//')
-set -x "AWS_SECRET_ACCESS_KEY" (grep -m 1 'aws_secret_access_key' ~/.aws/credentials | sed 's/.*= *//')
+set -x "AWS_ACCESS_KEY_ID" (grep -i -m 1 'aws_access_key_id' ~/.aws/credentials | sed 's/.*= *//')
+set -x "AWS_SECRET_ACCESS_KEY" (grep -i -m 1 'aws_secret_access_key' ~/.aws/credentials | sed 's/.*= *//')
