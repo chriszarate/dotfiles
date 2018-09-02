fafd6e405e238634d08992f7aaf3183df5cdf85c
chriszarate <chris@zarate.org>
2018-09-02 11:46:39 -0400

    fixx

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,7 @@ set fish_greeting ""
 set fish_key_bindings fish_vi_key_bindings
 set -x EDITOR vim
 
-# Set initial window title.
-echo -n -e "\033]0;~\007"
-
+# Source local config.
 for file in ~/.config/fish/inc/*.fish ~/.*.fish
   source $file
 end
@@ -13,7 +11,7 @@ end
 switch (echo $TERM)
   case 'screen*'
   case '*'
-    test -z "$ATOM_HOME"; and tmux attach -t (whoami) >/dev/null 2>&1; or tmux new-session -s (whoami)
+    status --is-interactive; and test -z "$ATOM_HOME"; and tmux attach -t (whoami) >/dev/null 2>&1; or tmux new-session -s (whoami)
 end
 
 # Connect to ssh-agent session (requires keychain).
