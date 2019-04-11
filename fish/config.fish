896bd8bb50244fade3e5017df323ab8d85dbcb53
chriszarate <chris@zarate.org>
2019-04-11 09:07:21 -0400

    trim trim

--- fish/config.fish
+++ fish/config.fish
@@ -3,12 +3,9 @@ set fish_key_bindings fish_vi_key_bindings
 set -x EDITOR vim
 
 # Source local config.
-for file in ~/.config/fish/inc/*.fish ~/.*.fish
+for file in ~/.config/fish/inc/*.fish
   source $file
 end
 
 # Open a tmux session if one doesn't exist.
 tmux has-session; or tmux new-session -s (whoami)
-
-# Connect to ssh-agent session (requires keychain).
-which keychain >/dev/null; and eval (keychain --eval --quiet --agents ssh --nogui)
