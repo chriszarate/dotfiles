5dbf52d69aa816a6aafe64cb5614c775a5309361
chriszarate <chris@zarate.org>
2019-04-20 23:20:12 -0400

    scratch

--- apt/install-repos.sh
+++ apt/install-repos.sh
@@ -10,7 +10,7 @@ sudo apt-get -y install \
 ## Docker
 if ! grep -q "^deb .*docker" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-	sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+	sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) nightly" # no stable for disco yet
 fi
 
 ## Pop! OS
--- bin/setup-homebrew.sh
+++ bin/setup-homebrew.sh
@@ -5,10 +5,18 @@ DOTFILES="$HOME/.dotfiles"
 
 # Install linuxbrew packages.
 if [ "$(uname)" = "Linux" ]; then
+  if ! command -v brew >/dev/null; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+  fi
+
   /home/linuxbrew/.linuxbrew/bin/brew bundle --file="$DOTFILES/brew/Brewfile-linux"
 fi
 
 # Install brew packages and casks
 if [ "$(uname)" = "Darwin" ]; then
+  if ! command -v brew >/dev/null; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
+
   brew bundle --file="$DOTFILES/brew/Brewfile"
 fi
