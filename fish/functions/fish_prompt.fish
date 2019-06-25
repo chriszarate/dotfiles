8dcf916d0a490508a4e766b5820f5d5c8bdbf33e
chriszarate <chris@zarate.org>
2019-06-25 00:24:24 -0400

    borderlands

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,40 +1,4 @@
-set __fish_git_prompt_describe_style 'branch'
-set __fish_git_prompt_show_informative_status true
-
-set __fish_git_prompt_color 666666
-set __fish_git_prompt_color_bare blue
-set __fish_git_prompt_color_branch yellow
-set __fish_git_prompt_color_branch_detached red
-set __fish_git_prompt_color_dirtystate red
-set __fish_git_prompt_color_invalidstate red
-set __fish_git_prompt_color_merging red
-set __fish_git_prompt_color_stagedstate green
-set __fish_git_prompt_color_stashstate blue
-set __fish_git_prompt_color_untrackedfiles red
-set __fish_git_prompt_color_upstream yellow
-
-set __fish_git_prompt_char_cleanstate ''
-set __fish_git_prompt_char_dirtystate ' ●'
-set __fish_git_prompt_char_invalidstate ' (╯°□°)╯︵'
-set __fish_git_prompt_char_stagedstate ' ●'
-set __fish_git_prompt_char_stashstate ' ⚑'
-set __fish_git_prompt_char_stateseparator ''
-set __fish_git_prompt_char_untrackedfiles ' ○'
-set __fish_git_prompt_char_upstream_ahead ' ▲'
-set __fish_git_prompt_char_upstream_behind ' ▼'
-set __fish_git_prompt_char_upstream_prefix ''
-
 function fish_prompt
-  set -l git_prompt (__fish_git_prompt '%s')
-  if [ "$git_prompt" != '' ]
-    echo ""
-    printf '%s%s %s' (set_color 666666) (prompt_pwd) $git_prompt
-    set -l git_stash (git stash list 2>/dev/null | wc -l | awk '{print $1}')
-    if [ "$git_stash" != '0' ]
-      printf '%s' "$___fish_git_prompt_color_stashstate$__fish_git_prompt_char_stashstate$git_stash"
-    end
-    echo ""
-  end
-
-  printf '%s> ' (set_color 666666)
+  git_status_tmux
+  printf '\n%s> ' (set_color 666666)
 end
---
+++ fish/functions/git_status_tmux.fish
@@ -0,0 +1,33 @@
+set __fish_git_prompt_describe_style 'branch'
+set __fish_git_prompt_show_informative_status true
+
+set __fish_git_prompt_char_cleanstate ''
+set __fish_git_prompt_char_dirtystate ' ◒'
+set __fish_git_prompt_char_invalidstate ' (╯°□°)╯︵'
+set __fish_git_prompt_char_stagedstate ' ●'
+set __fish_git_prompt_char_stashstate ' ◆'
+set __fish_git_prompt_char_stateseparator ''
+set __fish_git_prompt_char_untrackedfiles ' ○'
+set __fish_git_prompt_char_upstream_ahead ' ▲'
+set __fish_git_prompt_char_upstream_behind ' ▼'
+set __fish_git_prompt_char_upstream_prefix ''
+
+function git_status_tmux -d "Provide a simple but detailed git status in tmux."
+  set -l git_prompt (__fish_git_prompt '%s')
+  set -l git_stash '0'
+
+  if [ "$git_prompt" != '' ]
+    set git_stash (git stash list 2>/dev/null | wc -l | awk '{print $1}')
+    if [ "$git_stash" != '0' ]
+      set git_prompt "$git_prompt$__fish_git_prompt_char_stashstate$git_stash"
+    end
+
+    # Remove colors (it would be a pain to translate these for tmux).
+    set git_prompt (echo " $git_prompt " | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g")
+  end
+
+  # Update tmux pane border with git status instead of displaying repetitively
+  # in prompt.
+  tmux set-window-option "pane-border-format" "#{?pane_active,$git_prompt,}"
+end
+
