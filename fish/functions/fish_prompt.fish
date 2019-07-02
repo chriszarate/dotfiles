c4c940f69b6704c3854be35919632452e2fce066
chriszarate <chris@zarate.org>
2019-07-02 00:12:19 -0400

    colors pls

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,3 +1,3 @@
 function fish_prompt
-	printf '\n%s> ' (set_color 666666)
+	printf '\n%s\n%s┅┅%s ' (git_status) (set_color 504945) (set_color normal)
 end
--- fish/functions/git_status.fish
+++ fish/functions/git_status.fish
@@ -4,13 +4,9 @@ function git_status -d "Provide a simple but detailed git status in an environme
 	if [ "$output" != '' ]
 		set -l stash (git stash list 2>/dev/null | wc -l | awk '{print $1}')
 		if [ "$stash" != '0' ]
-			set -a -l output "$__fish_git_prompt_char_stashstate$stash"
+			set -a output (set_color $__fish_git_prompt_color_stashstate)"$__fish_git_prompt_char_stashstate$stash"
 		end
-
-		# Remove colors (it would be a pain to translate these outside of fish) and
-		# trim newlines.
-		echo $output | sed -e "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d "\n"
-	else
-		echo ''
   end
+
+	echo $output
 end
