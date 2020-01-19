2122ae9f1d93972f812cf58c7c374c402e3be954
chriszarate <chris@zarate.org>
2020-01-19 01:27:14 -0500

    dir

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,3 +1,3 @@
 function fish_prompt
-	printf '\n%s%s %s\n%s> %s' (set_color ebdbb2) (prompt_pwd) (git_status) (set_color ebdbb2) (set_color normal)
+	printf '\n%s%s %s\n%s> %s' (set_color 888888) (prompt_pwd) (git_status) (set_color ebdbb2) (set_color normal)
 end
