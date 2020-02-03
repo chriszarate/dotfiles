92718887f5728c73066f4c77507b99acc9fdb402
chriszarate <chris@zarate.org>
2020-02-03 12:27:38 -0500

    take a break

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,3 @@ set -x EDITOR vim
 for file in ~/.config/fish/inc/*.fish
   source $file
 end
-
-# Open a tmux session if one doesn't exist.
-tmux has-session >/dev/null 2>&1; or tmux new-session -s (whoami)
