31017bf976ccc4de6af1b1d617dd93d4c072f339
chriszarate <chris@zarate.org>
2020-04-30 22:30:38 -0400

    push it real good

--- fish/inc/homebrew.fish
+++
@@ -1,6 +0,0 @@
-if [ (uname) = 'Linux' ]
-	set -x HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew"
-	set -x HOMEBREW_CELLAR "$HOMEBREW_PREFIX/Cellar"
-	set -x HOMEBREW_REPOSITORY "$HOMEBREW_PRFIX/Homebrew"
-	set --universal fish_user_paths $fish_user_paths "$HOMEBREW_PREFIX/bin $HOMEBREW_PREFIX/sbin"
-end
