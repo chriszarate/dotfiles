74e00495071c338135398b3ad8caadd9808c5eec
chriszarate <chris@zarate.org>
2021-01-03 11:22:37 -0500

    cleanup

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,3 +1,19 @@
 function fish_prompt
-	printf '\n%s%s %s\n%s> %s' (set_color 888888) (prompt_pwd) (git_status) (set_color ebdbb2) (set_color normal)
+	printf '\n%s%s %s\n' (set_color 888888) (prompt_pwd) (__fish_git_prompt '%s')
+
+	switch $fish_bind_mode
+		case default
+			set_color --bold 83a598
+
+		case replace_one
+			set_color --bold 8ec07c
+
+		case visual
+			set_color --bold fe8019
+
+		case '*'
+			set_color normal
+	end
+
+	echo '> '
 end
