da7e68483bcd8a3bf3c35297b0a299dd35b324f4
chriszarate <chris@zarate.org>
2021-02-28 11:02:04 -0500

    arming

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,11 @@ else if test -x "/usr/local/bin/fish"
   set -x SHELL /usr/local/bin/fish
 end
 
+# Prefer ARM64 if available.
+if string match -q "*ARM64*" (uname -a)
+  set -x HOMEBREW_CHANGE_ARCH_TO_ARM 1
+end
+
 set -x EDITOR vim
 set -U fish_user_paths /opt/homebrew/bin /usr/local/bin $fish_user_paths
 
