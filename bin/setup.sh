a99928bbec4472903dd072ab162b748adc17dfd8
chriszarate <chris.zarate@automattic.com>
2022-01-02 19:05:09 -0500

    mixin

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 # Relative to home directory.
 DOTFILES="$HOME/.dotfiles"
 
-$DOTFILES/bin/setup-links.sh
-$DOTFILES/bin/setup-homebrew.sh
-$DOTFILES/bin/setup-fish.sh
-$DOTFILES/bin/setup-vim.sh
+"$DOTFILES/bin/setup-links.sh"
+"$DOTFILES/bin/setup-homebrew.sh"
+"$DOTFILES/bin/setup-fish.sh"
+"$DOTFILES/bin/setup-vim.sh"
