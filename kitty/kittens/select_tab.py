c3e710422ae2e6d1d235ca9f87e2e4589de47191
chriszarate <chris@zarate.org>
2022-01-16 10:49:05 -0500

    learn to python

--- kitty/kittens/select_tab.py
+++ kitty/kittens/select_tab.py
@@ -1,4 +1,5 @@
 from kitty.boss import Boss
+from kitty.tabs import Tab
 from typing import List, Union
 
 def main(args: List[str]):
@@ -17,24 +18,20 @@ def handle_result(args: List[str], answer: str, target_window_id: int, boss: Bos
             if tab.id == ans:
                boss.set_active_tab(tab)
 
-   # Build a list of tabs
-   tab_list = ()
+   def format_tab_title(tab: Tab) -> str:
+      title = tab.title
+      count = len(tab.windows)
 
-   # Add an option to open a new tab
-   tab_list = tab_list + ((0, '[New tab]'),)
-
-   for t in boss.all_tabs:
-      title = t.title
-
-      # Indicate if there is more than 1 window
-      count = len(t.windows)
       if count > 1:
-         title = title + ' [{count} windows]'.format(count=count)
+         title = f'{title} [{count} windows]'
 
-      # Mark the current tab with an indicator
-      if t.id == boss.active_tab.id:
-         title = title + ' [current tab]'
+      if tab.id == boss.active_tab.id:
+         title = f'{title} [current tab]'
 
-      tab_list = tab_list + ((t.id, title),)
+      return title
 
-   boss.choose_entry('Choose a tab to switch to', tab_list, chosen)
+   boss.choose_entry(
+       'Choose a tab to switch to',
+       ((t.id, format_tab_title(t)) for t in boss.all_tabs),
+       chosen
+   )
