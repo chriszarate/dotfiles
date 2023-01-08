fa13cadde50fbec96349e80d1d7615814192cc28
chriszarate <chris.zarate@automattic.com>
2023-01-08 08:38:10 -0500

    fix shit

--- kitty/kittens/select_tab.py
+++ kitty/kittens/select_tab.py
@@ -9,14 +9,17 @@ from kittens.tui.handler import result_handler
 @result_handler(no_ui=True)
 
 def handle_result(args: List[str], answer: str, target_window_id: int, boss: Boss) -> None:
-   def chosen(ans: Union[None, str, int]) -> None:
-      if ans == 0:
-         boss.new_tab()
+   current_tab = boss.active_tab
+   last_layout = current_tab.current_layout.name
 
+   def chosen(ans: Union[None, str, int]) -> None:
       if isinstance(ans, int):
          for tab in boss.all_tabs:
             if tab.id == ans:
                boss.set_active_tab(tab)
+               break
+      if last_layout != 'stack':
+         current_tab.last_used_layout()
 
    def format_tab_title(tab: Tab) -> str:
       title = tab.title
@@ -29,6 +32,8 @@ def handle_result(args: List[str], answer: str, target_window_id: int, boss: Bos
          title = f'{title} [current tab]'
 
       return title
+
+   current_tab.goto_layout('stack')
 
    boss.choose_entry(
       'Choose a tab to switch to or ESC to cancel',
