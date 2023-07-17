696f6b764421a65cac2c7fb93e77452c19130820
chriszarate <chris.zarate@automattic.com>
2023-07-16 22:14:13 -0400

    tick

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-fish_add_path /opt/homebrew/bin /opt/homebrew/sbin /usr/local/bin $HOME/.bin
+fish_add_path -m $HOME/.bin /opt/homebrew/bin /opt/homebrew/sbin /usr/local/bin
 
 set fish_greeting ""
 set -x EDITOR vim
