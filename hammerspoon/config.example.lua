a987e741ea9b9773ac04cc872bffc31befc3a687
chriszarate <chris.zarate@automattic.com>
2023-08-20 15:29:20 -0400

    close slack forever

---
+++ hammerspoon/config.example.lua
@@ -0,0 +1,5 @@
+-- copy this file to config.lua and edit
+
+local config = {}
+
+return config
--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,11 +1,18 @@
 -- luacheck: globals hs spoon
 
+-- load config
+local config = require 'config'
+
 -- define spoons and config, which will be passed to :start
 local spoons = {
 	ControlEscMap = {
 		timeout = 0.2,
 	},
 	HoldToQuit = {},
+	SlackNotifier = {
+		cookieToken = config.slackCookieToken,
+		workspaceToken = config.slackWorkspaceToken,
+	},
 }
 
 -- load spoons
