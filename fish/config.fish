26b1928d73c1ac9717a7cb8cf76f802e14f2a9c5
chriszarate <chris.zarate@automattic.com>
2024-01-06 13:19:05 -0500

    orbin

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,6 @@ set -x SSH_AUTH_SOCK ~/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/D
 
 # Don't rely on the order config files are parsed when setting $PATH, because it
 # can change without warning. Do this in deliberate order.
-fish_add_path --append --move $HOME/.bin /opt/homebrew/bin /opt/homebrew/sbin /usr/local/bin
+fish_add_path --append --move $HOME/.bin $HOME/.orbstack/bin /opt/homebrew/bin /opt/homebrew/sbin /usr/local/bin
 fnm env --use-on-cd --log-level error --shell fish | source
 zoxide init fish | source
