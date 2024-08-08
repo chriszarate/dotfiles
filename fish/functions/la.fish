2112912d39e58418fdafc181346db67ea353e82d
chriszarate <chris.zarate@automattic.com>
2024-08-08 14:08:04 -0400

    right prompt

--- fish/functions/la.fish
+++ fish/functions/la.fish
@@ -1,3 +1,4 @@
 function la --wraps=eza -d 'Use eza instead of ls'
+	set --erase -g last_known_pwd # force printing path in fish_right_prompt
 	eza --all --header --long $argv
 end
--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -1,3 +1,4 @@
 function ls --wraps=eza -d 'Use eza instead of ls'
+	set --erase -g last_known_pwd # force printing path in fish_right_prompt
 	eza $argv
 end
