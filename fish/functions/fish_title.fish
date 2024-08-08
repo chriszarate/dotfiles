fb2ed3bb2c6bd92436391166aac5f1937102abee
chriszarate <chris.zarate@automattic.com>
2024-08-08 14:07:30 -0400

    yy

--- fish/functions/fish_title.fish
+++ fish/functions/fish_title.fish
@@ -1,6 +1,10 @@
 function fish_title -d 'Set the title for the current terminal window'
 
+	# Use the current command or the first argument, if passed.
 	set -l command "$_"
+	if test -n "$argv[1]"
+		set command "$argv[1]"
+	end
 
   switch "$command"
 
@@ -19,5 +23,10 @@ function fish_title -d 'Set the title for the current terminal window'
 		case '*'
 			echo "$command"
 
+	end
+
+	# If a second argument is passed, consider it a file descriptor.
+	if test -n "$argv[2]"
+		printf ':%s' "$argv[2]"
 	end
 end
