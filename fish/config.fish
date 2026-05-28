12bb3cdd33824728050992bfc063a828ba7ccf50
chriszarate <chris@zarate.org>
2026-05-28 11:01:20 -0600

    shhh

--- fish/config.fish
+++ fish/config.fish
@@ -7,5 +7,5 @@ set -x SSH_AUTH_SOCK ~/.ssh/autoproxxy-ssh-agent.socket
 # Don't rely on the order config files are parsed when setting $PATH, because it
 # can change without warning. Do this in deliberate order.
 fish_add_path --append --move $HOME/.bin $HOME/.orbstack/bin /opt/homebrew/bin /opt/homebrew/sbin /usr/local/bin
-fnm env --use-on-cd --log-level error --shell fish | source
+fnm env --use-on-cd --log-level=quiet --shell=fish | source
 zoxide init fish | source
